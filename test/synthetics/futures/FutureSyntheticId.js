ca73a2255102d0ed6e24e2c2f50c34f060837df9
betswapexchange <69535002+betswapexchange@users.noreply.github.com>
2020-08-24 16:53:52 -0400

    Update FutureSyntheticId.js

--- test/synthetics/futures/FutureSyntheticId.js
+++ test/synthetics/futures/FutureSyntheticId.js
@@ -5,13 +5,12 @@ const { derivativeFactory } = require('../../utils/derivatives')
 
 const [ owner ] = accounts
 
-const FutureSyntheticId = contract.fromArtifact('FutureSyntheticId')
+const FutureSyntheticId = contract.fromArtifact('SportsFuturesDerivativeLogic')
 
 describe('FutureSyntheticId', function () {
   before(async () => {
     this.futureLogic = await FutureSyntheticId.new({ from: owner })
 
-    this.sensitivity = web3.utils.toWei('1', 'ether')
   })
 
   context('validateInput', () => {
@@ -20,8 +19,8 @@ describe('FutureSyntheticId', function () {
         margin: 0,
         endTime: ~~(Date.now() / 1000) + 60, // + 1 min
         params: [
-          220, // future price,
-          this.sensitivity
+          15
+        // team ID,
         ]
       })
 
@@ -34,7 +33,7 @@ describe('FutureSyntheticId', function () {
         margin: 2000,
         endTime: ~~(Date.now() / 1000) + 60, // + 1 min
         params: [
-          0, // future price
+          0, // team ID
           this.sensitivity
         ]
       })
@@ -48,8 +47,8 @@ describe('FutureSyntheticId', function () {
         margin: 2000,
         endTime: ~~(Date.now() / 1000) - 60, // - 1 min
         params: [
-          200, // strike price
-          this.sensitivity
+          15 // team ID
+
         ]
       })
 
@@ -62,8 +61,7 @@ describe('FutureSyntheticId', function () {
         margin: 2000,
         endTime: ~~(Date.now() / 1000) + 60, // + 1 min
         params: [
-          220, // future price
-          this.sensitivity
+          15// team ID
         ]
       })
 
@@ -78,15 +76,14 @@ describe('FutureSyntheticId', function () {
         margin: 2000,
         endTime: ~~(Date.now() / 1000) + 60, // + 1 min
         params: [
-          220, // future price
-          this.sensitivity
+          15, // team ID
         ]
       })
 
       const result = await this.futureLogic.getMargin(derivative, { from: owner })
       const buyerMargin = result.buyerMargin.toNumber()
       const sellerMargin = result.sellerMargin.toNumber()
-        
+
       assert.equal(buyerMargin, derivative.margin, 'Buyer margin is wrong')
       assert.equal(sellerMargin, derivative.margin, 'Seller margin is wrong')
     })
@@ -94,13 +91,12 @@ describe('FutureSyntheticId', function () {
 
   context('getExecutionPayout', () => {
     it('buyer should receive double margin if currentPrice > futurePrice and profit > margin', async () => {
-      const currentPrice = 4500
+      const currentPrice = 15
       const derivative = derivativeFactory({
         margin: 1000,
         endTime: ~~(Date.now() / 1000) + 60, // + 1 min
         params: [
-          3000, // future price
-          this.sensitivity
+          15, // Team ID
         ]
       })
 
@@ -108,19 +104,19 @@ describe('FutureSyntheticId', function () {
 
       const buyerPayout = result.buyerPayout.toNumber()
       const sellerPayout = result.sellerPayout.toNumber()
-        
+
       assert.equal(buyerPayout, 2 * derivative.margin, 'Buyer payout is wrong')
       assert.equal(sellerPayout, 0, 'Seller payout is wrong')
     })
 
     it('seller should receive double margin if currentPrice <= futurePrice and profit > margin', async () => {
-      const currentPrice = 1500
+      const currentPrice = 12
       const derivative = derivativeFactory({
         margin: 1000,
         endTime: ~~(Date.now() / 1000) + 60, // + 1 min
         params: [
-          3000, // future price
-          this.sensitivity
+          15 // team ID
+
         ]
       })
 
@@ -128,53 +124,11 @@ describe('FutureSyntheticId', function () {
 
       const buyerPayout = result.buyerPayout.toNumber()
       const sellerPayout = result.sellerPayout.toNumber()
-        
+
       assert.equal(buyerPayout, 0, 'Buyer payout is wrong')
       assert.equal(sellerPayout, 2 * derivative.margin, 'Seller payout is wrong')
     })
 
-    it('buyer should receive margin + profit and seller should receive the left margin if currentPrice > futurePrice and profit <= margin', async () => {
-      const currentPrice = 3500
-      const derivative = derivativeFactory({
-        margin: 1000,
-        endTime: ~~(Date.now() / 1000) + 60, // + 1 min
-        params: [
-          3000, // future price
-          this.sensitivity
-        ]
-      })
-
-      const profit = currentPrice - derivative.params[0]
-
-      const result = await this.futureLogic.getExecutionPayout(derivative, currentPrice, { from: owner })
-
-      const buyerPayout = result.buyerPayout.toNumber()
-      const sellerPayout = result.sellerPayout.toNumber()
-        
-      assert.equal(buyerPayout, derivative.margin + profit, 'Buyer payout is wrong')
-      assert.equal(sellerPayout, derivative.margin - profit, 'Seller payout is wrong')
-    })
-
-    it('seller should receive margin + profit and buyer should receive the left margin if currentPrice <= futurePrice and profit > margin', async () => {
-      const currentPrice = 2500
-      const derivative = derivativeFactory({
-        margin: 1000,
-        endTime: ~~(Date.now() / 1000) + 60, // + 1 min
-        params: [
-          3000, // future price
-          this.sensitivity
-        ]
-      })
-
-      const profit = derivative.params[0] - currentPrice
-
-      const result = await this.futureLogic.getExecutionPayout(derivative, currentPrice, { from: owner })
-
-      const buyerPayout = result.buyerPayout.toNumber()
-      const sellerPayout = result.sellerPayout.toNumber()
-        
-      assert.equal(buyerPayout, derivative.margin - profit, 'Buyer payout is wrong')
-      assert.equal(sellerPayout, derivative.margin + profit, 'Seller payout is wrong')
-    })
   })
+
 })
