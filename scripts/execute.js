7b1712422696e1087913be68b2f07d2c652c9e7b
MirthFutures <69535002+MirthFutures@users.noreply.github.com>
2020-09-15 23:13:19 -0400

    Update execute.js

--- scripts/execute.js
+++ scripts/execute.js
@@ -2,29 +2,55 @@ const Web3 = require('web3')
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 
 const CORE_ABI = require('./abis/Core.json')
+const ERC20_ABI = require('./abis/ERC20.json')
 
+const { derivativeFactory } = require('./utils/derivatives')
 // Constants
 const PUBLIC_KEY = ''
 const SEED_PHRASE = ''
 const PROVIDER = ''
 
 const OPIUM_CORE_ADDRESS = '0xE995d8E9E0a01c938e6ae5B05720Af245953dC57'
+const OPIUM_TOKEN_SPENDER_ADDRESS = '0xE39b9D5dC766102181D4C5Cd7df1691565B52032'
+const MARGIN_TOKEN_ADDRESS = '0x0558dc82f203C8c5f5b9033061aa5AdefFC40AF7'
+const tokenIds = ['73981377331712443496464688835913523576399119371292695783222430341724877019177', '71672154408559061657332570778143546654354803393112564850635294784036740919300']
 
-const Token_ID = [110363459063709181084178417231781641228662593473961330193795309800190701802147, 55187041287035530053929896967999705097985852671485563256481723344519621144736]
+const ORACLE_ID_ADDRESS = '0x9712b6aff7d2dB96097565EB8b2183b75e839130'
+const SYNTHETIC_ID_ADDRESS = '0xc34b9c9DBB39Be0Ef850170127A7b4283484f804'
 
 // Create instance of web3
 const web3 = new Web3(new HDWalletProvider(SEED_PHRASE, PROVIDER))
 
 // Create instances of contracts
 const opiumCore = new web3.eth.Contract(CORE_ABI, OPIUM_CORE_ADDRESS)
+const marginToken = new web3.eth.Contract(ERC20_ABI, MARGIN_TOKEN_ADDRESS)
+
+// Define derivative params
+
+// Using web3.utils.toWei() function to convert number to BigInt with 18 decimals, standard for DAI, won't work for USDC
+const margin = web3.utils.toWei('10') // 10 DAI ~ 10e18
+
+const endTime = ~~(Date.now() / 1000) + 10 * 60// 10 minutes from now
+
+const params = [ 15 ] // Params: [ Team Number ]
+
+const derivative = derivativeFactory({
+  margin,
+  endTime,
+  params,
+  oracleId: ORACLE_ID_ADDRESS,
+  token: MARGIN_TOKEN_ADDRESS,
+  syntheticId: SYNTHETIC_ID_ADDRESS
+})
 
 const quantity = 10
 
 // Start script
+  // Mint derivative
 const start = async () => {
-  // Execute derivative
+
   console.log(`Executing ${quantity} derivatives...`)
-  await opiumCore.methods.execute(Token_ID, quantity, [ PUBLIC_KEY, PUBLIC_KEY ]).send({ from: PUBLIC_KEY })
+  await opiumCore.methods.execute(tokenIds, [quantity, quantity], [derivative, derivative]).send({ from: PUBLIC_KEY, gas: 8000000 })
   console.log('Executed')
 }
 start()
