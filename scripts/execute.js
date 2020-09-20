4f1c9cf12d0776edb54ccbd846700b60313c9b61
MirthFutures <69535002+MirthFutures@users.noreply.github.com>
2020-09-20 14:00:10 -0400

    Update execute.js

--- scripts/execute.js
+++ scripts/execute.js
@@ -13,41 +13,32 @@ const PROVIDER = ''
 const OPIUM_CORE_ADDRESS = '0xE995d8E9E0a01c938e6ae5B05720Af245953dC57'
 const OPIUM_TOKEN_SPENDER_ADDRESS = '0xE39b9D5dC766102181D4C5Cd7df1691565B52032'
 const MARGIN_TOKEN_ADDRESS = '0x0558dc82f203C8c5f5b9033061aa5AdefFC40AF7'
-const tokenIds = ['', '']
+const tokenIds = ['33407272437444913482602580612601528193033053424995970071626926685056001637441', '46351917593465080552006059268398127402622384511764645862039246940722624725581']
 
-const ORACLE_ID_ADDRESS = ''
-const SYNTHETIC_ID_ADDRESS = ''
+
+const ORACLE_ID_ADDRESS = '0x60052c097bd2a653aea196fa8e9c378868dc24cb'
+const SYNTHETIC_ID_ADDRESS = '0xB84486eA8B34C65cD3bf3DeeB6990AA0a27a2EaF'
 
 // Create instance of web3
 const web3 = new Web3(new HDWalletProvider(SEED_PHRASE, PROVIDER))
 
 // Create instances of contracts
 const opiumCore = new web3.eth.Contract(CORE_ABI, OPIUM_CORE_ADDRESS)
-const marginToken = new web3.eth.Contract(ERC20_ABI, MARGIN_TOKEN_ADDRESS)
 
 // Define derivative params
-
-// Using web3.utils.toWei() function to convert number to BigInt with 18 decimals, standard for DAI, won't work for USDC
-const margin = web3.utils.toWei('10') // 10 DAI ~ 10e18
-
-const endTime = Date.UTC(2020, 8, 16, 3, 40, 0)
-
-const params = [ 15 ] // Params: [ Team Number ]
-
 const derivative = derivativeFactory({
-  margin,
-  endTime,
-  params,
-  oracleId: ORACLE_ID_ADDRESS,
-  token: MARGIN_TOKEN_ADDRESS,
-  syntheticId: SYNTHETIC_ID_ADDRESS
+  margin: '10000000000000000000',
+  endTime: 1600581000,
+  params: [ 15 ],
+  oracleId: '0x60052c097bd2a653aea196fa8e9c378868dc24cb',
+  token: '0x0558dc82f203C8c5f5b9033061aa5AdefFC40AF7',
+  syntheticId: '0xB84486eA8B34C65cD3bf3DeeB6990AA0a27a2EaF'
 })
 
 const quantity = 10
 
 // Start script
 const start = async () => {
-
 //execute derivative
   console.log(`Executing ${quantity} derivatives...`)
   await opiumCore.methods.execute(tokenIds, [quantity, quantity], [derivative, derivative]).send({ from: PUBLIC_KEY, gas: 8000000 })
